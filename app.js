d6b678e310ab83814d2ef6316acc004c390fc15d
Aman Dwivedi <19dwivediaman@gmail.com>
2021-07-07 23:44:15 +0530

    final

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ function createCard(pokemon) {
 		<img src="${pokemon.sprites.other.dream_world.front_default}" width="150" height="150" class="card-img-top mt-4" alt="image">
 
 		<div class="card-body">
-			<h5 class="card-title text-capitalize">${pokemon.name}</h5>
+			<h3 class="card-title text-capitalize">${pokemon.name}</h3>
 			<div class="badge p-2 m-1 bg-info text-light">Height: ${pokemon.height}</div>	
 			<div class="badge p-2 m-1 bg-info text-light">Weight: ${pokemon.weight}</div>		
 			<div class="badge p-2 m-1 bg-danger text-light">Experience: ${pokemon.base_experience}</div>	
